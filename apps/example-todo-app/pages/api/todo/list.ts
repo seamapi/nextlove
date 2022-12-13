b299a80fcc372b5f63b38191f109c7108550257f
Max Isom <hi@maxisom.me>
2022-12-13 13:26:42 -0800

    Handle query array params

---
+++ apps/example-todo-app/pages/api/todo/list.ts
@@ -0,0 +1,28 @@
+import { checkRouteSpec, withRouteSpec } from "lib/middlewares"
+import { z } from "zod"
+
+export const commonParams = z.object({
+  ids: z.array(z.string().uuid()),
+})
+
+export const route_spec = checkRouteSpec({
+  methods: ["GET"],
+  auth: "auth_token",
+  commonParams,
+  jsonResponse: z.object({
+    ok: z.boolean(),
+    todos: z
+      .object({
+        id: z.string().uuid(),
+      })
+      .array(),
+  }),
+})
+
+export default withRouteSpec(route_spec)(async (req, res) => {
+  const { ids } = req.commonParams
+
+  console.log({ ids })
+
+  return res.status(200).json({ ok: true, todos: ids.map((id) => ({ id })) })
+})
