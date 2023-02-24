14805117b550c1514516556f89064e3c7455466e
Itelo Filho <itelofilho@gmail.com>
2023-02-24 20:47:38 -0300

    fix: handle with zod effects

---
+++ apps/example-todo-app/tests/api/todo/list-with-refine.test.ts
@@ -0,0 +1,61 @@
+import { axiosAssert } from "tests/fixtures/axios-assert"
+import test from "ava"
+import getTestServer from "tests/fixtures/get-test-server"
+import { v4 as uuidv4 } from "uuid"
+
+test("GET /todo/list-with-refine", async (t) => {
+  const { axios } = await getTestServer(t)
+
+  axios.defaults.headers.common.Authorization = `Bearer auth_token`
+
+  const ids = [uuidv4(), uuidv4()]
+
+  const responseWithArray = await axios.get("/todo/list-with-refine", {
+    params: {
+      ids,
+    },
+  })
+
+  t.deepEqual(responseWithArray.data, {
+    ok: true,
+    todos: ids.map((id) => ({
+      id,
+    })),
+  })
+
+  const responseWithCommas = await axios.get("/todo/list-with-refine", {
+    params: {
+      ids: ids.join(","),
+    },
+  })
+
+  t.deepEqual(responseWithCommas.data, {
+    ok: true,
+    todos: ids.map((id) => ({
+      id,
+    })),
+  })
+
+  const responseWithTitle = await axios.get("/todo/list-with-refine", {
+    params: {
+      title: "title",
+    },
+  })
+
+  t.deepEqual(responseWithTitle.data, {
+    ok: true,
+    todos: [
+      {
+        id: "title",
+      },
+    ],
+  })
+
+  await axiosAssert.throws(t, async () => axios.get("/todo/list-with-refine"), {
+    status: 400,
+    error: {
+      type: "invalid_input",
+      message: "Either title or ids must be provided",
+    },
+  })
+})
