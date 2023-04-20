adc48bfdab76a62681d0a9ae665424fa00741621
Evan Sosenko <razorx@evansosenko.com>
2023-04-20 12:34:15 -0700

    Reproduce GET error

---
+++ apps/example-todo-app/pages/api/todo/get-no-validate-body.ts
@@ -0,0 +1,21 @@
+import {
+  checkRouteSpec,
+  withRouteSpec,
+  withRouteSpecWithoutValidateGetRequestBody,
+} from "lib/middlewares"
+import { NotFoundException } from "nextlove"
+import { TODO_ID } from "tests/fixtures"
+import { z } from "zod"
+
+export const route_spec = checkRouteSpec({
+  methods: ["GET", "POST"],
+  auth: "auth_token",
+  jsonBody: z.object({ name: z.string() }),
+  jsonResponse: z.object({
+    ok: z.boolean(),
+  }),
+})
+
+export default withRouteSpec(route_spec)(async (req, res) => {
+  return res.status(200).json({ ok: true })
+})
