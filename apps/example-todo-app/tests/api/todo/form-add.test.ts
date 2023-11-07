1d7593f3dd89a168702080781cf32da225cc20cf
kainpets <pawelstepniak22@gmail.com>
2023-11-07 14:00:22 +0100

    recreate formData not supporting z.object() inside test

--- apps/example-todo-app/tests/api/todo/form-add.test.ts
+++ apps/example-todo-app/tests/api/todo/form-add.test.ts
@@ -3,6 +3,8 @@ import { TODO_ID } from "tests/fixtures"
 import getTestServer from "tests/fixtures/get-test-server"
 import { v4 as uuidv4 } from "uuid"
 import { formData } from "pages/api/todo/form-add"
+import { withRouteSpec } from "lib/middlewares"
+import { z } from "zod"
 
 test("POST /todo/form-add", async (t) => {
   const { axios } = await getTestServer(t)
@@ -16,6 +18,37 @@ test("POST /todo/form-add", async (t) => {
     method: "POST",
     url: "/todo/form-add",
     data: bodyFormData,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  }).catch((err) => err)
+
+  t.is(successfulRes.status, 200)
+})
+
+test("Workspace supports an optional object of formData", async (t) => {
+  const { axios } = await getTestServer(t)
+
+  axios.defaults.headers.common.Authorization = `Bearer auth_token`
+
+  withRouteSpec({
+    auth: "support",
+    methods: ["GET", "POST"],
+    queryParams: z.object({
+      workspace_id: z.string(),
+    }),
+    formData: z.object([
+      z.any(),
+      z.object({
+        clear_sandbox_state: z.literal("clear_sandbox_state")
+      }),
+    ]).optional(),
+  } as const)
+
+  const successfulRes = await axios({
+    method: "POST",
+    url: "/todo/form-add",
+    data: formData,
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
