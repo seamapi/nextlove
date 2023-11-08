66d9b2f0c1ac046fd0d631fb09881516e6e10fac
kainpets <pawelstepniak22@gmail.com>
2023-11-08 13:08:32 +0100

    recreate issue in test

--- apps/example-todo-app/tests/api/todo/form-add.test.ts
+++ apps/example-todo-app/tests/api/todo/form-add.test.ts
@@ -4,7 +4,7 @@ import getTestServer from "tests/fixtures/get-test-server"
 import { v4 as uuidv4 } from "uuid"
 import { formData } from "pages/api/todo/form-add"
 import { withRouteSpec } from "lib/middlewares"
-import { z } from "zod"
+import * as  z from "zod"
 
 test("POST /todo/form-add", async (t) => {
   const { axios } = await getTestServer(t)
@@ -37,12 +37,14 @@ test("Workspace supports an optional object of formData", async (t) => {
     queryParams: z.object({
       workspace_id: z.string(),
     }),
-    formData: z.object([
-      z.any(),
-      z.object({
-        clear_sandbox_state: z.literal("clear_sandbox_state")
-      }),
-    ]).optional(),
+    formData: z
+      .object([
+        z.any(),
+        z.object({
+          clear_sandbox_state: z.literal("clear_sandbox_state"),
+        }),
+      ])
+      .optional(),
   } as const)
 
   const successfulRes = await axios({
