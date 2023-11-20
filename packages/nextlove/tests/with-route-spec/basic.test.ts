fa95a57823b922b3b9a87b0e94279388546f1e43
Mike Littman <mike@littman.im>
2023-11-20 14:39:35 -0500

    add test ensuring precedence of multiple auth error handlers

---
+++ packages/nextlove/tests/with-route-spec/basic.test.ts
@@ -0,0 +1,39 @@
+import test from "ava"
+import { createWithRouteSpec } from "../../src/with-route-spec"
+
+test("route-level onMultipleAuthMiddlewareFailures spec takes precedent", async (t) => {
+  let globalWasCalled = false
+  let routeWasCalled = false
+  const globalErrorHandler = () => void (globalWasCalled = true)
+  const routeErrorHandler = () => void (routeWasCalled = true)
+  const withRouteSpec = createWithRouteSpec({
+    apiName: "test",
+    productionServerUrl: "https://seam.com",
+    globalMiddlewares: [],
+    authMiddlewareMap: {
+      test: () => {
+        throw new Error("test")
+      },
+    },
+    onMultipleAuthMiddlewareFailures: globalErrorHandler,
+  })
+  const route = withRouteSpec({
+    methods: ["POST"],
+    auth: "test",
+    onMultipleAuthMiddlewareFailures: routeErrorHandler,
+    handler: () => void 0,
+  })
+  await t.notThrowsAsync(async () =>
+    route(async () => void 0)(
+      {} as any,
+      {
+        status() {
+          return { json() {} }
+        },
+      } as any
+    )
+  )
+
+  t.is(globalWasCalled, false)
+  t.is(routeWasCalled, true)
+})
