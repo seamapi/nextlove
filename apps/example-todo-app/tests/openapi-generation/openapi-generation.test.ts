ae26ab583ca79ea5265ecbf5ec291dbce5551477
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2024-06-28 11:20:37 +0200

    fix: Enhance OpenAPI Spec Generation for Nullable Union Types in Record Properties (#146)
    * Fix nullable params parsing
    
    * Test the fix
    
    * Fix tests
    
    * Enhance OpenAPI Spec Generation for Nullable Union Types in Record Properties
    
    * Test the fix
    
    * Fix tests
    
    * Improve test name

--- apps/example-todo-app/tests/openapi-generation/openapi-generation.test.ts
+++ apps/example-todo-app/tests/openapi-generation/openapi-generation.test.ts
@@ -10,7 +10,7 @@ test("should generate openapi with expected properties", async (t) => {
   t.deepEqual(typeof openapiJson, "object")
 })
 
-test("nullable param is correctly parsed", async (t) => {
+test("generateOpenAPI correctly parses nullable params", async (t) => {
   const openapiJson = JSON.parse(
     await generateOpenAPI({
       packageDir: ".",
@@ -25,4 +25,25 @@ test("nullable param is correctly parsed", async (t) => {
   t.truthy(descriptionParam)
   t.is(descriptionParam.type, "string")
   t.true(descriptionParam.nullable)
+})
+
+test("generateOpenAPI marks properties with null unions as nullable", async (t) => {
+  const openapiJson = JSON.parse(
+    await generateOpenAPI({
+      packageDir: ".",
+    })
+  )
+
+  const routeSpec = openapiJson.paths["/api/todo/add"].post
+  const metadataParam =
+    routeSpec.requestBody.content["application/json"].schema.properties.metadata
+
+  t.truthy(metadataParam)
+  t.is(metadataParam.type, "object")
+  t.true(metadataParam.additionalProperties.nullable)
+  t.true(
+    metadataParam.additionalProperties.oneOf.every(
+      (schema) => schema.type !== "null"
+    )
+  )
 })
