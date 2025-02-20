1f830ef84c02678f5ef5915d8d04840f331a40b1
Mike Wu <hello@mikewu.dev>
2025-02-21 03:47:41 +0900

    Update packages/nextlove/src/with-route-spec/middlewares/with-validation.ts
    Co-authored-by: Evan Sosenko <evan@getseam.com>

--- packages/nextlove/src/with-route-spec/middlewares/with-validation.ts
+++ packages/nextlove/src/with-route-spec/middlewares/with-validation.ts
@@ -92,7 +92,7 @@ const parseQueryParams = (
       if (isZodSchemaArray(value as z.ZodTypeAny)) {
         const array_input = input[key]
 
-        if (typeof array_input === "string" && !array_input) {
+        if (typeof array_input === "string" && array_input.length ===0) {
           parsed_input[key] = []
         }
 
